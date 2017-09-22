d29f704ad12dace870863a3f5ddba8403a47afa1
pet185377 <pet.185377@gmail.com>
2017-09-22 18:36:42 +0800

    bbr update-v4.13

--- bbr1.sh
+++ bbr1.sh
@@ -4,6 +4,6 @@ cd bnx2
 wget https://raw.githubusercontent.com/xjyxh1/rtinstall/master/bnx2/bnx2-mips-09-6.2.1b.fw
 wget https://raw.githubusercontent.com/xjyxh1/rtinstall/master/bnx2/bnx2-mips-06-6.2.3.fw
 cd ~
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-image-4.12.0-041200rc5-generic_4.12.0-041200rc5.201706112031_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.13/linux-image-4.13.0-041300-generic_4.13.0-041300.201709031731_amd64.deb
 dpkg -i linux-image-4.10*.deb
 update-grub
